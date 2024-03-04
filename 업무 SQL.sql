91f90eb693edd9bc88d6bb9b95ba78b03a960630
juniel1299 <62318700+juniel1299@users.noreply.github.com>
2024-03-04 14:53:43 +0900

    Update 업무 SQL.sql

--- 업무 SQL.sql
+++ 업무 SQL.sql
@@ -13,13 +13,143 @@
 -- 원준
 --b-6 
 
---b-7 
+-- 교육생 등록
+insert into tbltrainees
+values(
+1,'이채린','linear99','2049178','01012345678',sysdate,'우리은행','012-592-384171');
 
+--교육생 출력
+select t.name,t.ssn,t.tel,t.registrationdate,count(case
+       when tl.status = '수료' then 1 
+        end )as "수강횟수"
+        from tbltraineelist tl
+inner join tbltrainees t
+on tl.seq_trainee = t.seq_trainee
+group by t.name,t.ssn,t.tel,t.registrationdate;
+
+–- 수정
+update tblTrainees set ssn  = '2050692' where name = '이채린';
+
+–- 삭제
+delete from tblTrainees where name ='이채린’;
+
+-- 교육생 검색
+
+select c.name,oc.startdate,oc.enddate,r.name,tl.status,tl.day from tbltrainees t
+inner join tbltraineelist tl
+on t.seq_trainee = tl.seq_trainee
+inner join tblopencurriculum oc
+on tl.seq_opencurriculum = oc.seq_curriculum
+inner join tblcurriculum c
+on oc.seq_curriculum = c.seq_curriculum
+inner join tblRoom r
+on oc.seq_room = r.seq_room; 
+
+--b-7  ( 테이블 수정 중이라 못 넣음)
+-- 과목별
+select j.name,g.writtengrade,g.practicalgrade 
+from tblGrades g
+inner join tbltestschedule ts
+    on g.seq_testschedule = ts.seq_testschedule
+    inner join tblsubjectlist sl
+    on sl.seq_subjectlist = ts.seq_subjectlist
+    inner join tblsubject j
+    on s.seq_subject = ts.
+    group by j.name;
+   
+  --교육생 개인 별
+    select r.name,j.name,writtengrade,practicalgrade 
+from tblGrades g
+inner join tblsubjectlist s
+    on g.seq_subjectList = s.seq_subjectList
+    inner join tblsubject j
+    on j.seq_subject = s.seq_subject
+    inner join tbltraineelist t
+    on t.seq_traineeList = g.seq_traineelist
+    inner join tbltrainees r
+    on r.seq_trainee = t.seq_trainee
+    where r.name = '변소류';
+
+  
 --b-8 
 
---b-10 
+--출결 관리
+
+  --1. 개설 과정 별 
+select t.name,a.day,c.name,ad.situation from tblAttendance a
+inner join tblTraineeList tl
+on a.seq_traineeList = tl.seq_traineelist
+inner join tblopencurriculum oc
+on oc.seq_opencurriculum = tl.seq_opencurriculum
+inner join tblCurriculum c
+on c.seq_curriculum = oc.seq_opencurriculum
+inner join tblattendancestatus ad
+on ad.seq_attendancestatus = a.seq_attendancestatus
+inner join tblTrainees t
+on t.seq_trainee = tl.seq_trainee
+group by t.name,a.day,c.name,ad.situation;
+
+-- 2. 특정 인원 (where 문 ) 
+select tt.name,a.day,c.name,s.situation from tblAttendance a
+inner join tblTraineeList t
+on a.seq_traineeList = t.seq_traineelist
+inner join tblopencurriculum o
+on o.seq_opencurriculum = t.seq_opencurriculum
+inner join tblCurriculum c
+on c.seq_curriculum = o.seq_opencurriculum
+inner join tblattendancestatus s
+on s.seq_attendancestatus = a.seq_attendancestatus
+inner join tblTrainees tt
+on tt.seq_trainee = t.seq_trainee
+where tt.name = '제류혁';
+
+
+  --B-9
+  
+--교사 평가 조회 
+
+select t.name,e.grade,e.content from tblteacherevaluation e
+inner join tblopencurriculum o
+on e.seq_opencurriculum = o.seq_opencurriculum
+inner join tblteacher t
+on t.seq_teacher = o.seq_teacher;
+
+  --b-10 
+
+--교육 희망자 면접 기록 
+insert into tblinterviewresults
+values(
+1,1,'합격','자바에 대한 기초 지식이 있음');
+
+
+--교육 희망자 정보 조회 
+
+select t.name,t.id,t.ssn,t.tel from tbltrainees t
+inner join tblinterviewschedule s
+on s.seq_trainee = t.seq_trainee;
+
+-- 합격자 불합격자 구분
+select t.name,r.status from tbltrainees t
+inner join tblinterviewschedule s
+on s.seq_trainee = t.seq_trainee
+inner join tblinterviewresults r
+on r.seq_schedule = s.seq_schedule
+order by r.status;
 
 --b-11
+  
+--교재 추가 
+insert into tblTextbook
+values(
+20,'OpenCV-Python으로 배우는 영상처리 및 응용','생능');
+
+--교재 수정
+
+update tblTextbook set publisher  = '에이콘' where name = 'OpenCV-Python으로 배우는 영상처리 및 응용';
+
+--교재 삭제
+
+delete from tblTextbook where name ='OpenCV-Python으로 배우는 영상처리 및 응용';
 
 
 -- 원혁
