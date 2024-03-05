e7f79b2443d7716d284f05f2b59a50a25b92c6b2
wonhyuk21 <wonhyuk2101@gmail.com>
2024-03-05 16:18:50 +0900

    업무SQL완료

--- view.sql
+++ view.sql
@@ -67,7 +67,6 @@ as
                         inner join tblAttendanceStatus tas
                             on a.seq_attendanceStatus = tas.seq_attendanceStatus;
 
-select * from vwTrainees;
 -- 성적
 create or replace view vwGrades
 as
